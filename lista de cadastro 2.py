7f7704c84b44438f90ef7dc3c000551620beb0a7
Luiz Eduardo <124412405+LuizPimenteldev@users.noreply.github.com>
2023-05-08 22:13:56 -0300

    Update lista de cadastro 2.py
    cadastro dos jogadores não estava aparecendo no terminal, por isso foi necessário alteração neste código, agora está funcionando!!

--- lista de cadastro 2.py
+++ lista de cadastro 2.py
@@ -8,8 +8,14 @@ WIDTH = 800
 HEIGHT = 600
 BG_COLOR = (255, 255, 255)
 
-# Inicializa a tela
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+# Função que salva o nome do jogador
+def register_player(name1, name2, window):
+    # Salva o nome do jogador em um arquivo ou banco de dados, por exemplo
+    print("Jogador 1 cadastrado:", name1)
+    print ("Jogador 2 cadastrado: ", name2) 
+
+    # Fecha a janela de cadastro
+    window.destroy()
 
 # Função que exibe a janela de cadastro
 def show_register_window():
@@ -17,31 +23,30 @@ def show_register_window():
     register_window = tk.Tk()
     register_window.title("Cadastro de Jogadores")
 
-    # Cria um campo de texto para o nome do jogador
-    player_name_label = tk.Label(register_window, text="Nome do Jogador:")
-    player_name_label.pack()
-    player_name_entry = tk.Entry(register_window)
-    player_name_entry.pack()
+    # Cria um campo de texto para o nome do jogador 1
+    player_name_label_1 = tk.Label(register_window, text="Nome do Jogador 1:")
+    player_name_label_1.pack()
+    player_name_entry_1 = tk.Entry(register_window)
+    player_name_entry_1.pack()
+
+    # Cria um campo de texto para o nome do jogador 2
+    player_name_label_2 = tk.Label(register_window, text="Nome do Jogador 2:")
+    player_name_label_2.pack()
+    player_name_entry_2 = tk.Entry(register_window)
+    player_name_entry_2.pack()
 
     # Cria um botão para enviar o cadastro
     register_button = tk.Button(register_window, text="Cadastrar",
-                                command=lambda: register_player(player_name_entry.get(), register_window))
+                                command=lambda: register_player(name1=player_name_entry_1.get(), name2=player_name_entry_2.get(), window=register_window))
     register_button.pack()
 
     register_window.mainloop()
-
-# Função que salva o nome do jogador
-def register_player(name, window):
-    # Salva o nome do jogador em um arquivo ou banco de dados, por exemplo
-    print("Jogador cadastrado:", name)
-
-    # Fecha a janela de cadastro
-    window.destroy()
 
 # Exibe a janela de cadastro quando o jogo é iniciado
 show_register_window()
 
 # Loop principal do jogo
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 running = True
 while running:
     for event in pygame.event.get():
@@ -54,3 +59,4 @@ while running:
     pygame.display.update()
 
 pygame.quit()
+
