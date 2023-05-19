9648f8b5cad7234b5c02461986f1b7d6b68b1bdd
Luiz Eduardo <124412405+LuizPimenteldev@users.noreply.github.com>
2023-05-19 20:27:53 -0300

    Add files via upload
    Exemplos de Códigos pra sockets, apenas para termos uma ideia e entender mais sobre

---
+++ SOCKET_ex1.py
@@ -0,0 +1,9 @@
+#TCP protocol
+
+import socket #importa modulo socket
+TCP_IP = "<endereço ip>" # Endereço ip da máquina que será usada
+TCP_PORTA = "..." # Porta disponibilizada pelo servidor
+TAMANHO_BUFFER = '...'
+tabuleiro = "123456789"
+helper = "123456789"
+
